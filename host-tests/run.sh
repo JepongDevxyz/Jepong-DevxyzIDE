7ecdc2b72841585fce4097bc337eda56b80b39dd
JepongDevxyz <businessdevgithubjepong@gmail.com>
2026-09-16 14:21:02 +0800

    test: run offline build planning coverage

--- host-tests/run.sh
+++ host-tests/run.sh
@@ -17,6 +17,7 @@ javac -d "$OUT" \
   "$ROOT/host-tests/ProjectArchiveServiceHostTest.java" \
   "$ROOT/host-tests/ProjectImportServiceHostTest.java" \
   "$ROOT/host-tests/SafeZipCancellationHostTest.java" \
+  "$ROOT/host-tests/BuildPlannerHostTest.java" \
   "$ROOT/host-tests/EditorSessionHostTest.java" \
   "$ROOT/host-tests/EditorSettingsHostTest.java" \
   "$ROOT/host-tests/AppAppearanceSettingsHostTest.java" \
@@ -49,6 +50,7 @@ java -cp "$OUT" ProjectTemplateGeneratorHostTest
 java -cp "$OUT" ProjectArchiveServiceHostTest
 java -cp "$OUT" ProjectImportServiceHostTest
 java -cp "$OUT" SafeZipCancellationHostTest
+java -cp "$OUT" BuildPlannerHostTest
 java -cp "$OUT" EditorSessionHostTest
 java -cp "$OUT" EditorSettingsHostTest
 java -cp "$OUT" AppAppearanceSettingsHostTest
